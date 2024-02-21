a729bf0b25a0951e05f82c2523968c99cad87c6d
pholiveir <106701950+pholiveir@users.noreply.github.com>
2024-02-21 15:29:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const botoes = documento.queryselectorA11(".botao");
+const botoes = document.querySelectorAll(".botao");
 
-for(let i=;i <botoes.length;i++){
-   botoes[i].onclick=function(){
+for(let i=0;i <botoes.length;i++){
+  botoes[i].onclick = function(){
 
-     for(let j=0;j<botoes.length;j++){
-       
+       for(let j=0;j<botoes.length;j++){
+         botoes[i].classList.remove("ativo");
+       }
+    
+    botoes[i].classList.add("ativo");
+  }
+}
